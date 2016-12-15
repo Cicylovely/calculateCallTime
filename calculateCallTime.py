017dd3c92adb515289d2f2ba4464ccce349dfae0
Louisa <Cicy_lovely@hotmail.com>
2016-12-15 17:33:58 +0900

    通話時間のログをcsvファイルに書き出せるようになった。
    出力ファイルのFormat：
        空行
        ID:
        Name:
        Start Date:
        Start Time:
        End Date:
        End Time:
        空行
        ID　CALL_TIME　START_TIME　END＿TIME　DATE
    
    出力判定：
        ・実験初日の開始時間～最終日の終了時間の間である
        　　　　　　　　　　かつ
    　　・[51,0]と[50,0]が対になっていること
        　（[51,0]が連続に記録された場合：一番下のを記録する
        　　[50,0]が連続に記録された場合：一番上のを記録する）
        　　　　　　　　　　かつ
        ・通話開始日と終了日が同じ日であること
    
        by louisa
        2016.12.15

--- calculateCallTime.py
+++ calculateCallTime.py
@@ -1,5 +1,5 @@
 # For calculate call time
-# 2016.12.08
+# 2016.12.15
 # by louisa
 
 import csv
@@ -18,6 +18,8 @@ first_call_time_list = []
 last_call_time_list = []
 is_start_time_recorded = False
 is_end_time_recorded = False
+is_experiment_started = False
+is_experiment_ended = False
 startCallDate_list = []
 endCallDate_list = []
 start_time = ''
@@ -59,9 +61,9 @@ def get_experiment_schedule( temp_file = open(FILE_USER_DATA, 'r') ):
             first_day_list = item_list[1].split('.')
         elif item_list[0] == 'End Date:':
             last_day_list = item_list[1].split('.')
-        elif item_list[0] == 'Start Time':
+        elif item_list[0] == 'Start Time:':
             first_call_time_list = item_list[1].split(':')
-        elif item_list[0] == 'End Time':
+        elif item_list[0] == 'End Time:':
             last_call_time_list = item_list[1].split(':')
 
     temp_file.close()
@@ -106,35 +108,51 @@ def check_same_day(start_list, end_list):
 def cut_time_data(temp_data):
     temp_list1 = temp_data.split('T')
     temp_list2 = temp_list1[1].split('+')
-    time_data = temp_list2[0]
-    return time_data
+    time_list = temp_list2[0].split(':')
+    return time_list
 
 
-def cut_date_data(temp_data):
-    temp_list = temp_data.split('T')
+def cut_date_data(date_str):
+    temp_list = date_str.split('T')
     date_list = temp_list[0].split('-')
     return date_list
 
 
-def calculate_call_time(start_temp, end_temp):
-    start_temp = map(int, start_temp)
-    end_temp = map(int, end_temp)
-    hh = end_temp[0] - start_temp[0]
-    mm = end_temp[1] - start_temp[1]
-    ss = end_temp[2] - start_temp[2]
-
+def calculate_time(start_list, end_list):
+    start_list = map(int, start_list)
+    end_list = map(int, end_list)
+    hh = end_list[0] - start_list[0]
+    mm = end_list[1] - start_list[1]
+    ss = end_list[2] - start_list[2]
     if mm < 0:
         hh -= 1
         mm += 60
     if ss < 0:
         mm -= 1
         ss += 60
-
     hh_str = '%02d' % hh
     mm_str = '%02d' % mm
     ss_str = '%02d' % ss
     call_time = hh_str + ':' + mm_str + ':' + ss_str
     return call_time
+
+
+def compare2time_values(start_list, end_list):
+    start_list = map(int, start_list)
+    end_list = map(int, end_list)
+    hh = end_list[0] - start_list[0]
+    mm = end_list[1] - start_list[1]
+    ss = end_list[2] - start_list[2]
+    if mm < 0:
+        hh -= 1
+        mm += 60
+    if ss < 0:
+        mm -= 1
+        ss += 60
+    if hh >= 0 and mm >= 0 and ss >= 0:
+        return True
+    else:
+        return False
 
 
 def cut_call_data(log_list):
@@ -164,6 +182,7 @@ def cut_call_data(log_list):
         # for test
         #print log_list[1], log_list[2]
         endCallDate_list = date_list
+
         if is_start_time_recorded:
             is_same_day = check_same_day(startCallDate_list, endCallDate_list)
             if is_same_day:
@@ -174,15 +193,16 @@ def cut_call_data(log_list):
 
     if is_end_time_recorded:
         # Calculate how long a call lasted
-        call_time = calculate_call_time(start_time.split(':'), end_time.split(':'))
+        call_time = calculate_time(start_time, end_time)
 
         # Create output csv item list
         call_log_output = []
         call_log_output.append(call_id)
         call_log_output.append(call_time)
-        call_log_output.append(start_time)
-        call_log_output.append(end_time)
-        # Write call logs: [id, call_time, start_time, end_time]
+        call_log_output.append(start_time[0] + ':' + start_time[1] + ':' + start_time[2])
+        call_log_output.append(end_time[0] + ':' + end_time[1] + ':' + end_time[2])
+        call_log_output.append(date_list[0] + "." + date_list[1] + '.' + date_list[2])
+        # Write call logs: [id, call_time, start_time, end_time, date]
         csvWriter.writerow(call_log_output)
         #print 'call_log_output: ', call_log_output
 
@@ -198,10 +218,37 @@ def cut_call_data(log_list):
 # Main program
 init_output_file()
 get_experiment_schedule()
-
+line_no = 0
 for line in f_in:
     log_list = line.strip('\n').split('\t')
-    cut_call_data(log_list)
+    if line_no > 0:
+        date_list = cut_date_data(log_list[2])
+        time_list = cut_time_data(log_list[2])
+        if not is_experiment_started:
+            is_first_day = [False] * 3
+            for i in range(3):
+                if date_list[i] == first_day_list[i]:
+                    is_first_day[i] = True
+            if is_first_day.count(True) == 3:
+                if compare2time_values(first_call_time_list, time_list):
+                    is_experiment_started = True
+        else:
+            if not is_experiment_ended:
+                is_last_day = [False] * 3
+                for i in range(3):
+                    if date_list[i] == last_day_list[i]:
+                        is_last_day[i] = True
+                if is_last_day.count(True) == 3:
+                    if compare2time_values(last_call_time_list, time_list):
+                        is_experiment_ended = True
+                        print "is_experiment_ended = ", is_experiment_ended
+            else:
+                break
+        if is_experiment_started and not is_experiment_ended:
+                cut_call_data(log_list)
+    line_no += 1
 
 f_in.close()
 f_out.close()
+
+print "call_logs.cvs has been writen!"
